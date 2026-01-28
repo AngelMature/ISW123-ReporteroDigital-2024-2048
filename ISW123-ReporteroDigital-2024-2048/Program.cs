1650602765e20753f1d99d22ec698f2e92adbaee
ANGEL DAVID MATURE CARABALLO <am2024-2048@uce.edu.do>
2026-01-28 19:27:12 -0400

    Creación inicial del proyecto Reportero Digital

--- ISW123-ReporteroDigital-2024-2048/Program.cs
+++ ISW123-ReporteroDigital-2024-2048/Program.cs
@@ -14,14 +14,7 @@ namespace ReporteroDigital
 
             Reportero reportero = new Reportero();
 
-            reportero.ArticuloGenerado += mensaje =>
-            {
-                Console.WriteLine($"EVENTO: {mensaje}");
-            };
-
-            await reportero.GenerarArticuloAsync();
-
-            Console.WriteLine("Proceso finalizado.");
+           
         }
     }
 }
