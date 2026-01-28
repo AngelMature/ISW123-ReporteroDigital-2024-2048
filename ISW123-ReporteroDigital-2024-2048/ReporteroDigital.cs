95be52117b04e50051223215c5ced17856531b12
ANGEL DAVID MATURE CARABALLO <am2024-2048@uce.edu.do>
2026-01-28 19:32:49 -0400

    Implementado paralelismo con Task.WhenAll

--- ISW123-ReporteroDigital-2024-2048/ReporteroDigital.cs
+++ ISW123-ReporteroDigital-2024-2048/ReporteroDigital.cs
@@ -30,22 +30,6 @@ namespace ReporteroDigital
             }
         }
 
-        private async Task<string> ObtenerTextoAsync()
-        {
-            await Task.Delay(1000);
-            return " Texto principal de la noticia.";
-        }
-
-        private async Task<string> ObtenerImagenAsync()
-        {
-            await Task.Delay(1200);
-            return " Imagen cargada correctamente.";
-        }
-
-        private async Task<string> ObtenerAnalisisAsync()
-        {
-            await Task.Delay(800);
-            throw new FuenteNoDisponibleException("No se pudo obtener el análisis.");
-        }
+       
     }
 }
